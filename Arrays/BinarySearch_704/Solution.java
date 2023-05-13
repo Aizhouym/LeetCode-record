cef63dcc47f78413cff89313b6b8e218060a2859
周英明 <2931338821@qq.com>
2023-05-13 20:08:16 +0800

    Solution

--- Arrays/BinarySearch_704/Solution.java
+++ Arrays/BinarySearch_704/Solution.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Solution {
 
-    public static void main(String[] args) {
-        
+    public int getIndex(int[] num, int target){
+        int low, high, mid;
+        low = 0;
+        high = num.length-1;
+    
+        while(low<=high){
+            mid = (low+high)/2;
+            if( target < num[mid] ){
+                high = mid - 1;
+            }else if( target == num[mid] ){
+                return mid;
+            }else if( target > num[mid] ){
+                low = mid+1;
+            }
+        }//Binary Search
+
+        return -1;
     }
 
 
-}
+    public static void main(String[] args) {
     
+        Solution solution = new Solution();
+        int[] num ={1,4,9,10,17,25,29,35,40,45,48,75};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入target:");
+        int target = sc.nextInt();
+        System.out.println(solution.getIndex(num, target));
+        sc.close();
+    
+    }
+}
+
+
 
