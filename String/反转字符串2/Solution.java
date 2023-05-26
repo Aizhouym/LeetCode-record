f755668a50f8f9ea3776671a50c376ed7b494550
周英明 <2931338821@qq.com>
2023-05-26 21:23:40 +0800

    changes

--- String/反转字符串2/Solution.java
+++ String/反转字符串2/Solution.java
@@ -1,7 +1,21 @@
 
 
 public class Solution {
-    public void reverseStr(String s, int k){
-        
+    public String reverseStr(String s, int k){
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i+=2*k){
+            int start = i;
+            int end = Math.min(ch.length - 1, start + K -1);
+
+            while (start < end){
+                char tmp = ch[start];
+                ch[start] = ch[end];
+                ch[end] = ch[start];
+                start++;
+                end--;
+            }
+        }
+
+        return new String(ch);
     }
 }
